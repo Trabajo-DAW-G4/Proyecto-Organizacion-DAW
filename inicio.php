39afb4edbfc86a09e5a327f9074d32b4b5cd7b5a
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2024-02-29 12:25:13 +0100

    Estilos pagina inicial abierta a modificaciones

--- inicio.php
+++ inicio.php
@@ -15,18 +15,41 @@ if (!isset($_SESSION["id"]) || !isset($_SESSION["user"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
 </head>
 
-<body>
-    <h1>Bienvenido, <?php echo $_SESSION["user"]; ?></h1>
+<body class="bg-gradient-to-r from-gray-100 to-gray-200">
+<header>
+    <nav class="bg-gray-900 py-4 flex items-center justify-between">
     
-    <button onclick="location.href='./user.php'">Todos los Productos</button>
-    <button onclick="location.href='./camisetas.php'">Camisetas</button>
-    <button onclick="location.href='./pantalones.php'">Pantalones</button>
-    <button onclick="location.href='./sudaderas.php'">Sudaderas</button>
+        <img src="./assets/J3AT-removebg-preview.png" alt="Logo" class="h-16 ml-6">
+    
+    <ul class="flex m-5 gap-5">
+        <li>
+            <a href="#" class="text-white hover:text-gray-300">Inicio</a>
+        </li>
+        <li>
+            <a href="#" class="text-white hover:text-gray-300">Sobre nosotros</a>
+        </li>
+        <li>
+            <a href="#" class="text-white hover:text-gray-300">Términos y privacidad</a>
+        </li>
+    </ul>
+</nav>
+    </header>
+    <h1 class="text-center m-10 text-3xl font-bold mb-4">Bienvenido, <?php echo ucfirst($_SESSION["user"]); ?></h1>
+    <div class="m-10 flex flex-col items-center gap-5">
+    <button onclick="location.href='./user.php'" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-6 rounded w-full mb-2">Todos los Productos</button>
+    <button onclick="location.href='./camisetas.php'" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-6 rounded w-full mb-2">Camisetas</button>
+    <button onclick="location.href='./pantalones.php'" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-6 rounded w-full mb-2">Pantalones</button>
+    <button onclick="location.href='./sudaderas.php'" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-6 rounded w-full mb-2">Sudaderas</button>
+    <a href="./logout.php" class='bg-red-500 hover:bg-red-600 text-white font-bold py-4 px-6 text-center rounded w-full'>Cerrar Sesión</a>
 
-    <br>
-    <a href="./logout.php">Cerrar Sesión</a>
+
+
+
+
 </body>
 
 </html>
