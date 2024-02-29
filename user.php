f94a99352cf91de76c9e0ab52994cc7b4e119904
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2024-02-29 13:12:47 +0100

    Estilos nuestros productos(mejorable)

--- user.php
+++ user.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include("./connectDB.php");
 
 session_start();
@@ -17,15 +17,33 @@ if ($conn) {
     $statement = $conn->prepare($query);
     $statement->execute();
     $productos_ropa = $statement->fetchAll(PDO::FETCH_ASSOC);
-    ?>
+?>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <header>
+        <nav class="bg-gray-900 py-4 flex items-center justify-between">
 
-    <h2>Nuestros Productos</h2>
+            <img src="./assets/J3AT-removebg-preview.png" alt="Logo" class="h-16 ml-6">
 
-    <div class="container-menu">
-        <?php foreach($productos_ropa as $producto): ?>
+            <ul class="flex m-5 gap-5">
+                <li>
+                    <a href="#" class="text-white hover:text-gray-300">Inicio</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white hover:text-gray-300">Sobre nosotros</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white hover:text-gray-300">Términos y privacidad</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <h2 class="text-3xl font-bold text-center my-8">Nuestros Productos</h2>
+
+    <div class="container-menu grid grid-cols-1 md:grid-cols-3 gap-6 m-5">
+        <?php foreach ($productos_ropa as $producto) : ?>
             <figure class="figure">
-                <img src="<?= $producto['ruta_foto'] ?>" alt="<?= $producto['nombre'] ?>" width="200" height="200">
-                <figcaption><?= $producto['nombre'] ?> - $<?= $producto['precio'] ?></figcaption>
+                <img src="<?= $producto['ruta_foto'] ?>" alt="<?= $producto['nombre'] ?>" class="w-full h-auto object-cover border-4 rounded-md border-black">
+                <figcaption class="text-center"><?= $producto['nombre'] ?> - $<?= $producto['precio'] ?></figcaption>
                 <form method="post" action="orders.php">
                     <input type="hidden" name="product_id" value="<?= $producto['id'] ?>">
                 </form>
@@ -33,43 +51,47 @@ if ($conn) {
         <?php endforeach; ?>
     </div>
 
-    <?php if(count($productos_ropa) > 0): ?>
-        <form method="post" action="orders.php">
-            <table border="1">
-                <tr>
-                    <th>Nombre</th>
-                    <th>Precio</th>
-                    <th>Talla</th>
-                    <th>Stock</th>
-                    <th>Cantidad</th>
-                </tr>
-                <?php foreach($productos_ropa as $producto): ?>
-                    <tr>
-                        <td><?= $producto['nombre'] ?></td>
-                        <td><?= $producto['precio'] ?></td>
-                        <td><?= $producto['talla'] ?></td>
-                        <td><?= $producto['stock'] ?></td>
-                        <td>
-                            <input type="number" name="quantity[<?= $producto['id'] ?>]" value="0" min="0" max="<?= $producto['stock'] ?>">
-                            <?php if ($producto['stock'] <= 0): ?>
-                            <?php endif; ?>
-                        </td>
+    <?php if (count($productos_ropa) > 0) : ?>
+        <form method="post" action="orders.php" class="mt-8">
+            <table class="w-full border-collapse border border-gray-300">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="px-6 py-4">Nombre</th>
+                        <th class="px-6 py-4">Precio</th>
+                        <th class="px-6 py-4">Talla</th>
+                        <th class="px-6 py-4">Stock</th>
+                        <th class="px-6 py-4">Cantidad</th>
                     </tr>
-                <?php endforeach; ?>
-                <tr>
-                    <td colspan="5"><input type="submit" value="Realizar Pedido"></td>
-                </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($productos_ropa as $producto) : ?>
+                        <tr class="border-b border-gray-300">
+                            <td class="text-center px-4 py-2"><?= $producto['nombre'] ?></td>
+                            <td class="text-center px-4 py-2"><?= $producto['precio'] ?></td>
+                            <td class="text-center px-4 py-2"><?= $producto['talla'] ?></td>
+                            <td class="text-center px-4 py-2"><?= $producto['stock'] ?></td>
+                            <td class="text-center px-4 py-2">
+                                <input type="number" name="quantity[<?= $producto['id'] ?>]" value="0" min="0" max="<?= $producto['stock'] ?>" class="w-2/3 border border-gray-300 rounded-md px-2 py-1">
+                                <?php if ($producto['stock'] <= 0) : ?>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    <tr>
+                        <td colspan="5" class="text-center py-4"><input type="submit" value="Realizar Pedido" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"></td>
+                    </tr>
+                </tbody>
             </table>
         </form>
-    <?php else: ?>
-        <p>No se encontraron resultados</p>
+    <?php else : ?>
+        <p class="text-center m-6 text-3xl">No se encontraron resultados</p>
     <?php endif; ?>
 
     <form method="post" action="logout.php">
-        <button type="submit">Cerrar Sesión</button>
+        <button type="submit" class='bg-red-500 hover:bg-red-600 text-white font-bold p-3 text-center rounded w-full'>Cerrar Sesión</button>
     </form>
 
-    <?php
+<?php
     $conn = null;
 } else {
     echo "Error al conectar a la Base de datos";
