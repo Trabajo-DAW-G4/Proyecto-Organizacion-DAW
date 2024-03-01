4d2bef8f8849dafb77a091c79669d67d5fc373ac
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2024-03-01 09:03:39 +0100

    Modificaciones login.php

--- login.php
+++ login.php
@@ -54,55 +54,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
-
-<body  class="flex flex-col items-center justify-center gap-6 p-10 max-w-lg mx-auto rounded bg-gradient-to-r from-red-300 via-red-400 to-red-500">
+<body class="bg-gradient-to-r from-red-300 via-red-400 to-red-500">
+    <header>
+    <nav class="bg-gray-900 py-4 flex items-center justify-between">
     
-    <form method="post" class="flex flex-col items-center justify-center gap-6 bg-red-200 p-10 max-w-lg mx-auto rounded">
+        <img src="./assets/J3AT-removebg-preview.png" alt="Logo" class="h-16 ml-6">
+    
+    <ul class="flex m-5 gap-5">
+        <li>
+            <a href="#" class="text-white hover:text-gray-300">Sobre nosotros</a>
+        </li>
+        <li>
+            <a href="#" class="text-white hover:text-gray-300">Términos y privacidad</a>
+        </li>
+    </ul>
+</nav>
+    </header>
+    <form method="post" class="flex flex-col items-center justify-center gap-6 bg-red-200 p-10 max-w-lg mx-auto rounded m-6">
         <label for="username" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full">Nombre de usuario:</label>
-        <input type="text" name="username" id="username" class="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-red-500">
+        <input type="text" class="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-red-500" name="username" id="username">
         <label for="password" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-full">Contraseña:</label>
-        <input type="password" name="password" id="password" class="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-red-500">
+        <input type="password" class="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-red-500" name="password" id="password">
         <button type="submit" class='bg-red-500 hover:bg-red-600 text-white font-bold p-3 text-center rounded w-full'>Iniciar Sesión</button>
         <a href="./index.php" class='bg-blue-500 hover:bg-blue-600 text-white text-center font-bold p-3 rounded w-full'>Registrarse</a>
     </form>
-
-    <script>
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./*.html'],
-  theme: {
-    container: {
-      padding: '1.4rem'
-    },
-    extend: {
-      spacing: {
-        'quarter': '25%',
-        'half': '50%',
-      },
-      colors: {
-        "do-blue-dark": "#080C2D",
-        "do-blue-medium": "rgb(0, 125, 255)",
-        "do-blue-light": "rgb(0, 105, 255)"
-      },
-      boxShadow: {
-        'input': '0 5px 1px 0 rgb(0 0 0 / 10%)',
-        'input-focus': '0 2px 1px 0 rgb(0 0 0 / 10%)',
- 
-      },
-      fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-        cascadia: ["Cascadia Code", "monospace"]
-      },
-      backgroundImage: theme => ({
-        'imagen-fondo': "url('./assets/fondo.avif')"
-      })
-    },
-  },
-  plugins: [],
-}
-    </script>
-
-
+</main>
 </body>
-
 </html>
